bea01b9b75505fda134ca78b3ff2cbcf58573236
Harley <harley_silva@icloud.com>
2025-11-23 13:53:48 -0300

    Adicionando Exercicio 1

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿/* == Pedir para um usuario digitar dois numero e mostrar o resultado da soma entre eles == */
+/* Meu objetivo é atraves de exercicos construir uma logica de programação e algoritmo solida,
+atraves de exercicios que vão dificultando ao longo do tempo. */
+
+//Pedindo para o usuario digita um numero
+Console.WriteLine("Digite um numero: ");
+
+//Salvando o numero digitado na variavel numero1
+int numero1 = int.Parse(Console.ReadLine());
+
+//Pedindo para o usuario digitar outro numero
+Console.WriteLine("Digite outro numero: ");
+
+//Salvando o numero digitado na variavel numero2
+int numero2 = int.Parse(Console.ReadLine());
+
+//Criando uma variavel que armazena o resultado da soma entre numero1 e numero2
+int resultado = numero1 + numero2;
+
+Console.WriteLine($"O resultado da soma: {numero1} + {numero2} = {resultado}");
