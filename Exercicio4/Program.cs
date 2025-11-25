0e874838efd132ee69467378be890e4e730115ad
Harley <harley_silva@icloud.com>
2025-11-25 19:10:47 -0300

    Adicionado exercicio 4

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,16 @@
+﻿int idade;
+string separador = "-------------------";
+
+Console.Write("Digite a sua idade: ");
+idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine(separador);
+
+if (idade >= 18)
+{
+    Console.WriteLine("Você é adulto");
+}
+else
+{
+    Console.WriteLine("Você ainda é menor");
+}
