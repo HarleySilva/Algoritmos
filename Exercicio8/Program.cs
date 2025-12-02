4431561aa5585acefb3ac3bba68079b7d329d4b5
Harley <harley_silva@icloud.com>
2025-12-02 17:29:13 -0300

    Adicionado Exercicio 8

---
+++ Exercicio8/Program.cs
@@ -0,0 +1,10 @@
+﻿int numero, numeroant, numerodep;
+
+Console.Write("Escreva um numero: ");
+numero = int.Parse(Console.ReadLine());
+
+numeroant = numero - 1;
+
+numerodep = numero + 1;
+
+Console.WriteLine($"{numeroant} {numero} {numerodep}");
